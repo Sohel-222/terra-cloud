2d5a3a97d6dd2ee3f4ab29f09db22fddb6a11b32
Sohel Shaikh <104219521+Sohel-222@users.noreply.github.com>
2025-05-17 11:56:29 +0530

    Create main.tf
    simple code to check terra cloud

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+ }
+ 
+resource "aws_instance" "myec2" {
+  ami           = "ami-0f88e80871fd81e91"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Web_Server"
+  }
+}
+
+resource "aws_instance" "yourec2" {
+  ami           = "ami-0f88e80871fd81e91"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "App_Server"
+  }
+}
+
+
